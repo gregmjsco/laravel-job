90452a6845c2900543cb65e4e3607e3e4e500287
Greg Johnston <gregmj.sco@gmail.com>
2025-01-12 16:34:57 +0900

    Remove default welcome screen and add placeholder home view which the router will now use HomeController to display

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Jobs in Japan</title>
+</head>
+<body class="bg-gray-200">
+    <x-header/>
+
+    <main class="container mx-auto p-4 mt-4">
+        @yield('content')
+    </main>
+    
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,2 @@
+<h1>Welcome to Jobs in Japan</h1>
+<p>Let's find your dream job in Japan</p>
