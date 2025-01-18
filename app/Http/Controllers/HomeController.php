56a71088a3523e63143c092d5222bf3468b14609
Greg Johnston <gregmj.sco@gmail.com>
2025-01-18 15:28:46 +0900

    Update home page to display 6 most recent job cards

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\Job;
 
 class HomeController extends Controller
 {
     //
-    public function index()
+    public function index(): View
     {
-        return view('pages.index');
+        $jobs = Job::latest()
+            ->limit(6)
+            ->get();
+
+        return view('pages.index')->with('jobs', $jobs);
     }
 }
