a2d05654795143ade48e9541165065c01f84c256
Greg Johnston <gregmj.sco@gmail.com>
2025-01-21 10:19:29 +0900

    Make non-required fields conditionally display on job cards component and in show page

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -39,15 +39,18 @@
               <li class="mb-2">
                 <strong>Site Location:</strong> {{$job->city}}, {{$job->prefecture}}
               </li>
+              @if($job->tags)
               <li class="mb-2">
                 <strong>Tags:</strong>
                 {{ ucwords(str_replace(',', ', ', $job->tags)) }}
               </li>
+              @endif
             </ul>
           </div>
         </div>
   
         <div class="container mx-auto p-4">
+        @if($job->requirements || $job->benefits)
           <h2 class="text-xl font-semibold mb-4">Job Details</h2>
           <div class="rounded-lg shadow-md bg-white p-4">
             <h3 class="text-lg font-semibold mb-2 text-blue-500">
@@ -59,6 +62,7 @@
             </h3>
             <p>{{$job->benefits}}</p>
           </div>
+        @endif
           <p class="my-5">
             Put "Job Application" as the subject of your email and attach your
             resume.
@@ -78,16 +82,22 @@
 
       <aside class="bg-white rounded-lg shadow-md p-3">
         <h3 class="text-xl text-center mb-4 font-bold">Company Info</h3>
+        @if($job->company_logo)
         <img
           src="/images/{{$job->company_logo}}"
           alt="{{$job->company_name}}"
           class="w-full rounded-lg mb-4 m-auto"
         />
+        @endif
         <h4 class="text-lg font-bold">{{$job->company_name}}</h4>
+        @if($job->company_description)
         <p class="text-gray-700 text-lg my-3">{{$job->company_description}}</p>
+        @endif
+        @if($job->company_website)
         <a href="{{$job->company_website}}" target="_blank" class="text-blue-500"
           >Visit Website</a
         >
+        @endif
       
         <a
           href=""
