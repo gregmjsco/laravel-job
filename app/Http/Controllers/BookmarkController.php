b830e3ff42ffec431961502458f62b3bd23a59f7
Greg Johnston <gregmj.sco@gmail.com>
2025-01-22 16:33:33 +0900

    Add functionality to remove bookmarks by adding delete route and creating destroy method in BookmarkController. Markup for button updated to check if job listing aleady saved and will render accordingly

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -20,4 +20,51 @@ class BookmarkController extends Controller
 
         return view('jobs.bookmarked')->with('bookmarks', $bookmarks);
     }
+
+    // @desc   Store a bookmark
+    // @route  POST /bookmarks/{job}
+    public function store(Job $job): RedirectResponse
+    {
+        $user = Auth::user();
+
+        // Check if the job is already bookmarked
+        if (
+            $user
+                ->bookmarkedJobs()
+                ->where('job_id', $job->id)
+                ->exists()
+        ) {
+            return back()->with('status', 'Job is already bookmarked.');
+        }
+
+        // Create a new bookmark
+        $user->bookmarkedJobs()->attach($job->id);
+
+        return back()->with('status', 'Job bookmarked successfully.');
+    }
+
+    // @desc   Remove a bookmark
+    // @route  DELETE /bookmarks/{job}
+    public function destroy(Job $job): RedirectResponse
+    {
+        $user = Auth::user();
+
+        // Check if the job is bookmarked before trying to remove it
+        if (
+            !$user
+                ->bookmarkedJobs()
+                ->where('job_id', $job->id)
+                ->exists()
+        ) {
+            return back()->with('error', 'Job is not bookmarked.');
+        }
+
+        // Remove the bookmark
+        $user->bookmarkedJobs()->detach($job->id);
+
+        return back()->with(
+            'status',
+            'Job removed from bookmarks successfully.'
+        );
+    }
 }
